6e7b7c85b169ba8ac7ef253c2d4f0eaa1d6dc0ee
Thu Nguyen <thu.nguyen@thistle.co>
2021-10-20 00:41:22 -0700

    Update minor version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='galley_api',
-    version='0.0.1',
+    version='0.1.0',
     packages=['galley'],
     install_requires=['sgqlc==14.0', 'mypy==0.770']
 )
