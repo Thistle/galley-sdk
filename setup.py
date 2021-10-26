1224d855b4d7242ad67c39bf5031f922a48c8ad0
Patrick Mounts <patrick.mounts@thistle.co>
2021-10-26 15:41:28 -0700

    bump galley_sdk version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='galley_sdk',
-    version='0.7.0',
+    version='0.6.0',
     packages=['galley'],
     install_requires=['sgqlc==14.0', 'backoff==1.11.1']
 )
