7dce31d120414bc21f81cd0c54e6ce166f883583
Patrick Mounts <patrick.mounts@thistle.co>
2021-10-26 15:40:57 -0700

    dynamically load dotenv if library present in galley/__init__.py

--- galley/__init__.py
+++ galley/__init__.py
@@ -1,4 +1,12 @@
+
 import os
+import importlib
+
+# instantiation of package dependencies that are only required for local development.
+dotenvlib = importlib.find_loader('dotenv')
+if dotenvlib is not None:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 api_key = os.getenv('GALLEY_API_KEY')
 api_url = os.getenv('GALLEY_URL')
