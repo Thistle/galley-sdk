696acc67a760f7714c9992a15353468a379c21fb
Patrick Mounts <patrick.mounts@thistle.co>
2021-10-26 15:46:05 -0700

    add comment to setup()

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-
+# Note: The only packages that should be added to the install_requires
+# are dependencies that are required on remote installs, not local development.
 setup(
     name='galley_sdk',
     version='0.6.0',
