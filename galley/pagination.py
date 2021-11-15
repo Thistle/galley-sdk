904673c0352b79ce5f697be241fc08e5a20c8983
Peter James <peter@thistle.co>
2021-11-15 11:17:08 -0800

    [ENG-857] nesting wrapper and updating tests

--- galley/pagination.py
+++ galley/pagination.py
@@ -3,22 +3,32 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def paginate_results(func, page_size: int = 25):
+def paginate_results(page_size=25):
     """
     can decorate any function with an initial list input
     and collect its output in pages of page_size
     """
 
-    def wrapper(*args):
-        input_list = args[0]
-        result = []
-        if type(input_list) is not list:
-            logger.error(f'argument {input_list} is not a list')
-            return result
-        for partition in range((len(input_list)//page_size) + 1):
-            result.extend(
-                func(input_list[partition * page_size: (partition + 1) * page_size])
-            )
-        return result
+    def decorating_function(func):
+        def wrapper(*args):
+            result = []
 
-    return wrapper
+            try:
+                input_list = args[0]
+            except IndexError:
+                logger.error('this function requires an argument')
+                return result
+
+            if type(input_list) is not list:
+                logger.error(f'arg {input_list} is not a list')
+                return result
+
+            for partition in range((len(input_list) // page_size) + 1):
+                print('making request')
+                chunk = input_list[partition * page_size: (partition + 1) * page_size]
+                if len(chunk) > 0:
+                    result.extend(func(chunk))
+            return result
+        return wrapper
+
+    return decorating_function
--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -7,18 +7,18 @@ class TestPaginateResults(TestCase):
     def setUp(self):
         self.mock = mock.MagicMock()
 
-    def test_called_5_for_100(self):
-        paginate_results(self.mock)(list(range(100)))
-        self.assertEqual(5, self.mock.call_count)
+    def test_called_4_for_100(self):
+        paginate_results()(self.mock)(list(range(100)))
+        self.assertEqual(4, self.mock.call_count)
 
-    def test_called_11_for_100_when_page_size_10(self):
-        paginate_results(self.mock, page_size=10)(list(range(100)))
-        self.assertEqual(11, self.mock.call_count)
+    def test_called_10_for_100_when_page_size_10(self):
+        paginate_results(page_size=10)(self.mock)(list(range(100)))
+        self.assertEqual(10, self.mock.call_count)
 
-    def test_called_1_for_empty_list(self):
-        paginate_results(self.mock, page_size=10)([])
-        self.assertEqual(1, self.mock.call_count)
+    def test_called_0_for_empty_list(self):
+        paginate_results()(self.mock)([])
+        self.assertEqual(0, self.mock.call_count)
 
     def test_nocall_noexception_for_bad_arg(self):
-        paginate_results(self.mock, page_size=10)(5)
+        paginate_results()((self.mock))(5)
         self.assertEqual(0, self.mock.call_count)
