52b2779662d29957ea8bcccc956a8231c56eaa4d
Peter James <peter@thistle.co>
2021-11-15 12:52:58 -0800

    (ENG-857) adding more edge cases to tests

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -7,13 +7,21 @@ class TestPaginateResults(TestCase):
     def setUp(self):
         self.mock = mock.MagicMock()
 
-    def test_called_4_for_100(self):
+    def test_called_4_for_100_by_default(self):
         paginate_results()(self.mock)(list(range(100)))
         self.assertEqual(4, self.mock.call_count)
+
+    def test_called_5_for_105_by_default(self):
+        paginate_results()(self.mock)(list(range(105)))
+        self.assertEqual(5, self.mock.call_count)
 
     def test_called_10_for_100_when_page_size_10(self):
         paginate_results(page_size=10)(self.mock)(list(range(100)))
         self.assertEqual(10, self.mock.call_count)
+
+    def test_called_11_for_105_when_page_size_10(self):
+        paginate_results(page_size=10)(self.mock)(list(range(105)))
+        self.assertEqual(11, self.mock.call_count)
 
     def test_called_0_for_empty_list(self):
         paginate_results()(self.mock)([])
