99d57f4f47fb710bb90e1851b26bf7e329d362f5
Thu Nguyen <thu.nguyen@thistle.co>
2021-11-16 14:34:39 -0800

    Refactor paginate_results decorator to account for invalid arg inputs

--- galley/pagination.py
+++ galley/pagination.py
@@ -25,11 +25,11 @@ def paginate_results(page_size=25):
                 return result
 
             for partition in range((len(input_list) // page_size) + 1):
-                try:
-                    chunk = input_list[partition * page_size: (partition + 1) * page_size]
-                    result.extend(func(chunk)) if len(chunk) > 0 else None
-                except Exception as e:
-                    logger.error(e)
+                chunk = input_list[partition * page_size: (partition + 1) * page_size]
+                funk = func(chunk) if chunk else None
+                if funk:
+                    result.extend(funk)
+
             return result
         return wrapper
     return decorating_function
