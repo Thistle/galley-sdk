aad1c62c3376db90046b5d87671700c70e2899ca
Dhrubajyoti Das <drew.das@thistle.co>
2022-01-03 10:58:51 -0800

    Update setup.py to 0.21.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 # are dependencies that are required on remote installs, not local development.
 setup(
     name='galley_sdk',
-    version='0.19.2',
+    version='0.21.1',
     packages=['galley'],
     install_requires=['sgqlc==14.0', 'backoff==1.11.1']
 )
