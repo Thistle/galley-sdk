29e48dfa192db79a94a82b9e421cbdb318e7c10c
Dip Dhanesha <dip@thistle.co>
2022-01-27 14:59:54 -0800

    [ENG-1011] Formatted file to have pep8 coding standards

--- galley/enums.py
+++ galley/enums.py
@@ -7,11 +7,13 @@ class MenuCategoryEnum(Enum):
     """
     MENU_TYPE = 'Y2F0ZWdvcnk6MjQ2NQ=='
 
+
 class MenuItemCategoryEnum(Enum):
     """
     Enum for categories, for item type menu item <category name>: <category id>
     """
     PRODUCT_CODE = 'Y2F0ZWdvcnk6MjQ2Nw=='
+
 
 class PreparationEnum(Enum):
     """
