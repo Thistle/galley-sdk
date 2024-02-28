7d718dcd1fe2b0699d90120bbda93b9bd270d62a
Chase DuBois <chase.dubois@thistle.co>
2024-02-28 16:05:58 -0600

    adjust setup.py to include galley.client subpackage

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Note: The only packages that should be added to the install_requires
 # are dependencies that are required on remote installs, not local development.
 setup(
     name='galley_sdk',
     version='0.46.0',
-    packages=['galley'],
+    packages=find_packages('galley/'),
     install_requires=['sgqlc==14.0', 'backoff==1.11.1']
 )
