5b9ce5351c9cd4695a6c8bf0ca7322b2623fef91
Chase DuBois <chase.dubois@thistle.co>
2024-02-28 16:09:42 -0600

    try another way

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Note: The only packages that should be added to the install_requires
 # are dependencies that are required on remote installs, not local development.
 setup(
     name='galley_sdk',
     version='0.46.0',
-    packages=find_packages('galley/'),
+    packages=['galley', 'galley.client'],
     install_requires=['sgqlc==14.0', 'backoff==1.11.1']
 )
